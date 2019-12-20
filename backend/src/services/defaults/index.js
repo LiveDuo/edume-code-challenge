7fa3bc77df14abf1fbec1218526b1818ac76e525
unknown <andy0n7@gmail.com>
2019-12-20 20:53:36 +0000

    change backend port to not collide with frontend dev-server

--- backend/src/services/defaults/index.js
+++ backend/src/services/defaults/index.js
@@ -2,10 +2,10 @@ const env = process.env.NODE_ENV || 'development'
 
 const configEnvs = {
 	development: {
-		defaultPort: 3000
+		defaultPort: 4000
 	},
 	production: {
-		defaultPort: 3000
+		defaultPort: 4000
 	}
 }
 
