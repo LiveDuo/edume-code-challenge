2c3f426650b5d8b08ba94a893acaeef8f694ac13
unknown <andy0n7@gmail.com>
2019-12-21 00:59:16 +0000

    added filtered parameter to frontend

--- frontend/src/services/store/actions.js
+++ frontend/src/services/store/actions.js
@@ -1,6 +1,6 @@
 const setWords = (nums) => {
   return async (dispatch) => {
-    const response = await fetch(`/v1/t9/calculate?nums=${nums}`)
+    const response = await fetch(`/v1/t9/calculate?nums=${nums}&filtered=true`)
     const data = await response.json()
     dispatch({type: 'SET_WORDS', words: data})
   }
