e7b4a6c69235f6f1c137fa3288073cbceaf814f6
unknown <andy0n7@gmail.com>
2019-12-21 04:42:06 +0000

    fixed App.js render test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+
+import thunk from 'redux-thunk'
+
+const mockStore = configureStore([thunk])
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const store = mockStore({words: null})
+  const { container } = render(<Provider store={store}><App/></Provider>);
+  expect(container).toBeInTheDocument();
 });
