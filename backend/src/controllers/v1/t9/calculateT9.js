f3ae1fef816b467152be1ee2665f5d5de3535bdc
unknown <andy0n7@gmail.com>
2019-12-21 01:00:23 +0000

    added filtering parameter and node cache

--- backend/src/controllers/v1/t9/calculateT9.js
+++ backend/src/controllers/v1/t9/calculateT9.js
@@ -1,14 +1,31 @@
-import httpStatusCode from '../../../services/http/statusCodes'
+import httpStatusCodes from '../../../services/http/statusCodes'
 
 import { t9 } from '../../../services/helpers/t9'
 import { combineArrays } from '../../../services/helpers/arrays'
 
 import { wordlist } from '../../../services/dictionary'
 
-const calculateT9 = (req, res) => {
-	const input = req.query.nums
-	const results = input.split('').reduce((allArr, arr) => combineArrays(allArr, t9[arr]), [''])
+import { getCachedThenQuery } from '../../../services/caching'
+
+const calculateT9Promise = async (nums, filtered) => {
+	const results = nums.split('').reduce((allArr, arr) => combineArrays(allArr, t9[arr]), [''])
+
 	const resultsFiltered = results.filter(result => wordlist.includes(result))
-	return res.status(httpStatusCode('OK')).send(resultsFiltered)
+	if (filtered) {
+		return resultsFiltered
+	} else {
+		return results
+	}
+}
+
+const calculateT9 = async (req, res) => {
+	const nums = req.query.nums
+	const filtered = (req.query.filtered !== 'false') //defaults to false
+
+	if (!nums)
+		return res.status(httpStatusCodes('Bad Request')).end('Missing parameter')
+
+	const result = await getCachedThenQuery('get-t9-'+nums+'-'+filtered, calculateT9Promise(nums, filtered))
+	return res.status(httpStatusCodes('OK')).send(result)
 }
 export { calculateT9 }
